32994532cefcc3d570f14153cbd83bc0a33f16f1
Qiao Yu <691761071@qq.com>
2019-01-26 17:24:06 +0800

    add all 4 layouts

--- app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.uiwidgettest;
 
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,95 +14,45 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText editText;
-    private ImageView imageView;
-    private ProgressBar progressBarCircle;
-    private ProgressBar progressBarBar;
-    private int img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        Button buttonAlert = (Button) findViewById(R.id.button_alert);
-        Button buttonProgress = (Button) findViewById(R.id.button_progress);
-        editText = (EditText) findViewById(R.id.edit_text);
-        imageView = (ImageView) findViewById(R.id.image_view);
-        progressBarBar = (ProgressBar) findViewById(R.id.progress_bar_bar) ;
-        progressBarCircle = (ProgressBar) findViewById(R.id.progress_bar_circle) ;
+        Button buttonLinear = (Button) findViewById(R.id.button_linear);
+        Button buttonRelative = (Button) findViewById(R.id.button_relative);
+        Button buttonFrame = (Button) findViewById(R.id.button_frame);
+        Button buttonPercent = (Button) findViewById(R.id.button_percent);
 
-        buttonAlert.setOnClickListener(this);   //类内的onClick接口作为监听函数
-        buttonProgress.setOnClickListener(this);
-        button.setOnClickListener(this);
-        img=1;
+        buttonLinear.setOnClickListener(this);   //类内的onClick接口作为监听函数
+        buttonRelative.setOnClickListener(this);
+        buttonFrame.setOnClickListener(this);
+        buttonPercent.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            //第一个按钮：换图，显示文字，显示进度条等功能
-            case R.id.button:
-                //换图
-                if(img == 1)
-                {
-                    imageView.setImageResource(R.drawable.img_2);
-                    img=2;
-                }
-                else
-                {
-                    imageView.setImageResource(R.drawable.img_1);
-                    img=1;
-                }
-                //显示输入框的内容
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this, inputText,
-                        Toast.LENGTH_SHORT).show();
-                //显示圆形进度条
-                if (progressBarCircle.getVisibility() == View.GONE) {
-                    progressBarCircle.setVisibility(View.VISIBLE);
-                } else {
-                    progressBarCircle.setVisibility(View.GONE);
-                }
-                //长条形进度条
-                int progress = progressBarBar.getProgress();
-                progress = progress + 10;
-                progressBarBar.setProgress(progress);
+            case R.id.button_linear:
+                Intent intent = new Intent(MainActivity.this, LinearActivity.class);
+                startActivity(intent);
                 break;
-            //第二个按钮：显示alert dialog
-            case R.id.button_alert:
-                AlertDialog.Builder dialog = new AlertDialog.Builder (MainActivity.
-                        this);
-                dialog.setTitle("This is Dialog");
-                dialog.setMessage("Something important.");
-                dialog.setCancelable(false);
-                dialog.setPositiveButton("OK", new DialogInterface.
-                        OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }});
-                dialog.setNegativeButton("Cancel", new DialogInterface.
-                        OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                });
-                dialog.show();
+
+            case R.id.button_relative:
+                Intent intent2 = new Intent(MainActivity.this, RelativeActivity.class);
+                startActivity(intent2);
                 break;
-            //第三个按钮：显示progress dialog
-            case R.id.button_progress:
-                ProgressDialog progressDialog = new ProgressDialog
-                        (MainActivity.this);
-                progressDialog.setTitle("This is ProgressDialog");
-                progressDialog.setMessage("Loading...");
-                progressDialog.setCancelable(true);
-                progressDialog.show();
-                /*注意， 如果在setCancelable() 中传入了false ，
-                表示ProgressDialog是不能通过Back键取消掉的，
-                这时你就一定要在代码中做好控制，
-                当数据加载完成后必须要调用ProgressDialog的dismiss() 方法来关闭对话框，
-                否则ProgressDialog将会一直存在。*/
+
+            case R.id.button_frame:
+                Intent intent3 = new Intent(MainActivity.this, FrameActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.button_percent:
+                Intent intent4 = new Intent(MainActivity.this, PercentActivity.class);
+                startActivity(intent4);
                 break;
             default:
                 break;
         }
-    }}
+    }
+}
